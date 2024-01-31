acf0d133ff49cd7df3db3f4afa404fdf16a97720
KJ Price <kjprice12@gmail.com>
2024-01-31 13:16:23 -0600

    Simplify JsonObjectMapper handlers

--- src/kjpy/transforms/json_object_mapper.py
+++ src/kjpy/transforms/json_object_mapper.py
@@ -1,4 +1,5 @@
 from typing import Callable, List, Optional, Union
+from enum import Enum
 
 
 def _ensure_list(items) -> List[str]:
@@ -11,25 +12,26 @@ def _split_keys(item: str) -> List[str]:
     return item.split(".")
 
 
+class CustomHandler(Enum):
+    PERSON = "is_person_field"
+    PUBLISHER = "is_publisher_field"
+    VARIATIONS = "is_variations_field"
+    VARIATION_OF = "is_variation_of_field"
+
+
 class JsonObjectMapper:
     def __init__(
         self,
         return_field_names: Union[str, List[str]],
         clean_fn: Optional[Callable] = None,
-        is_person_field=False,
-        is_publisher_field=False,
-        is_variations_field=False,
-        is_variation_of_field=False,
+        custom_handler: Optional[CustomHandler] = None,
     ):
-        self.is_person_field = is_person_field
-        self.is_publisher_field = is_publisher_field
-        self.is_variations_field = is_variations_field
-        self.is_variation_of_field = is_variation_of_field
         self.return_fields: List[str] = _ensure_list(return_field_names)
         self.return_fields_keys: List[List[str]] = [
             _split_keys(f) for f in self.return_fields
         ]
         self.clean_fn = clean_fn
+        self.custom_handler = custom_handler
 
     def get(self, value):
         if self.clean_fn:
