f7061a4cbbbc1cc26a8dcdae085f37b3cba642ea
KJ Price <kjprice12@gmail.com>
2024-01-31 13:15:37 -0600

    Remove debugging print

--- tests/transforms/test_transform_field_helpers.py
+++ tests/transforms/test_transform_field_helpers.py
@@ -39,7 +39,6 @@ class TestSetObjFromKey(unittest.TestCase):
 
     def test_set_obj_from_key_map(self):
         self.assertEqual(self.helper("tom", "jerry"), {**self.obj, "tom": "jerry"})
-        print(self.helper("Bob", "jerry"))
         self.assertEqual(self.helper("Bob", "jerry"), {**self.obj, "Bob": "jerry"})
         self.assertEqual(
             self.helper("Sara", "jerry"), {**self.obj, "Sara": ["Jim", "jerry"]}
