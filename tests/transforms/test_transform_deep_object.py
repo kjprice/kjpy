61c21762ce31644d098874ffec1b0b6cebf3ed61
KJ Price <kjprice12@gmail.com>
2024-01-31 17:33:16 -0600

    add more info

--- tests/transforms/test_transform_deep_object.py
+++ tests/transforms/test_transform_deep_object.py
@@ -3,26 +3,23 @@ import unittest
 from src.kjpy.transforms.transform_deep_object import RecordHandler
 from src.kjpy.transforms.json_object_mapper import JsonObjectMapper
 
-# TODO: Create test for transforming object
 
 original_object = {
     "name": "Bob",
-    "other_info": {
-        "phoneNumber": 111,
-        #  "tags": ["accountant", "balding"]
-    },
-    "meatadata": "Copyright",
+    "other_info": {"phoneNumber": 111, "tags": ["accountant", "balding"]},
+    "metadata": "Copyright",
 }
 
-expected_output = {"name": "Bob", "phone": 111}
+expected_output = {"name": "Bob", "phone": 111, "tags": ["accountant", "balding"]}
 
 fields_map = {
     "name": JsonObjectMapper("name"),
     "other_info": {
         "phoneNumber": JsonObjectMapper("phone"),
+        "tags": JsonObjectMapper("tags"),
     },
 }
-fields_to_ignore = ["meatadata"]
+fields_to_ignore = ["metadata"]
 
 
 class TestTransformDeepObject(unittest.TestCase):
