69c54255605b6f5c64cb5f8eb7358cdd3a1438d2
KJ Price <kjprice12@gmail.com>
2024-02-01 09:02:37 -0600

    Install paths

--- bin/install.sh
+++ bin/install.sh
@@ -8,4 +8,9 @@ cd ..
 # $pypath -m pip install --no-build-isolation --editable .
 /Users/kjprice/anaconda3/envs/py3.10/bin/python -m pip install --editable .
 /Users/kjprice/anaconda3/envs/ml/bin/python -m pip install --editable .
+/Users/kjprice/anaconda3/bin/python -m pip install --editable .
+
+# OOPS - should probably reset this
+/Users/kjprice/anaconda3/envs/kjpy/bin/python -m pip install --editable .
+
 # /Users/kjprice/anaconda3/envs/py3.10/bin/python -m pip uninstall kjpy
